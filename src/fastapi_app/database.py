3ef0f5867578bba3ca27da81ffab7dfd8b93fdfd
tomit4 <mosssap@gmail.com>
2024-08-22 03:50:31 -0700

    :pencil2: Fixed small commenting out

--- src/fastapi_app/database.py
+++ src/fastapi_app/database.py
@@ -18,10 +18,10 @@ SQLALCHEMY_DATABASE_URL = (
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # SQLite Configuration
-#  SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
-#  engine = create_engine(
-#  SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-#  )
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+# engine = create_engine(
+# SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
